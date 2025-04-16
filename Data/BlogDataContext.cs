62e84ad1b1fe19fe0ca3a211a5f8c6b90a6ab9cc
Maxwell Xavier <maxwelldearaujo1017@gmail.com>
2025-04-16 13:37:11 -0300

    Adicao dos Models e conexao com o banco

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,20 @@
+using FluentBlog.Data.Mappings;
+using FluentBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentBlog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTags { get; set; }
+        // public DbSet<Role> Roles { get; set; }
+        // public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost,1433;Database=FluentBlog;User ID=sa;Password=1q2w3e4r@#$");
+    }
+}
